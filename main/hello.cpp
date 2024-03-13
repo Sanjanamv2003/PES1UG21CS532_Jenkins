62e8d729bb55d8c22aad736fdc551aaea7cd63f2
M V Sanjana <128384399+Sanjanamv2003@users.noreply.github.com>
2024-03-13 12:45:03 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I am PES1UG21CS532_SanjanaMV\n";
   return 0;
 }
